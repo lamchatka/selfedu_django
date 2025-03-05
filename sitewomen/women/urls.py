2787338f75bbe7e4e8c7c20ef52a3892efa08d98
lamchatka <maxprolife@icloud.com>
2025-03-05 15:04:01 +0300

    Маршрутизация: Перенаправление (redirect). Функция reverse. Пользовательский конвертер

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path, include
-from . import views
+from django.urls import path, register_converter
+from . import views, converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index),  # http://127.0.0.1:8000 - доменное имя
-    path('cats/<int:cat_id>/', views.categories),  # http://127.0.0.1:8000/cat/ - url-адрес
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('', views.index, name='home'),  # http://127.0.0.1:8000 - доменное имя
+    path('cats/<int:cat_id>/', views.categories, name='cat_by_id'),  # http://127.0.0.1:8000/cat/ - url-адрес
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cat_by_slug'),
+    path('archive/<year4:year>/', views.archive, name='archive')
 
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -17,6 +18,14 @@ def categories_by_slug(request, cat_slug):
     if request.GET:
         print(request.GET)  # <QueryDict: {'name': ['Gagarina'], 'type': ['pop']}>
     return HttpResponse(f"<h1>Страница категорий</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2025:
+        uri = reverse('cat_by_slug', args=('music',))
+        return redirect(uri)
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>year: {year}</p>")
 
 
 def page_not_found(request, exception):
