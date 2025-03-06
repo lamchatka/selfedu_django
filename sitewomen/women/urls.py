b7d040fd3280fd730d2567ac63a148006367fffc
lamchatka <maxprolife@icloud.com>
2025-03-06 12:26:45 +0300

    Шаблоны, начало. Функции render() и render_to_string(). Передача данных (переменных) в шаблоны

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000 - доменное имя
     path('cats/<int:cat_id>/', views.categories, name='cat_by_id'),  # http://127.0.0.1:8000/cat/ - url-адрес
     path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cat_by_slug'),
-    path('archive/<year4:year>/', views.archive, name='archive')
+    path('archive/<year4:year>/', views.archive, name='archive'),
+    path('about/', views.about, name='about'),
 
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,13 +1,37 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from django.urls import reverse
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 
 def index(request):
     """
     Возвращает главную страницу
     """
-    return HttpResponse("Hello world!")
+    # t = render_to_string("women/index.html")
+    # return HttpResponse(t)
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 2, 3, 2, 5},
+        'dct': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
@@ -29,4 +53,4 @@ def archive(request, year):
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
