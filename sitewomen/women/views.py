8ce8de0014320a3758ea60b96c124630c12dd825
lamchatka <maxprolife@icloud.com>
2025-03-07 23:40:27 +0300

    Теги шаблонов. Теги if и for

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,11 +5,11 @@ from django.urls import reverse
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
-
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 
 def index(request):
@@ -25,7 +25,7 @@ def index(request):
         'lst': [1, 2, 'abc', True],
         'set': {1, 2, 3, 2, 5},
         'dct': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20),
+        'posts': data_db
     }
     return render(request, 'women/index.html', context=data)
 
